b6414d8e90678fcf70accc0ee2a1effb70b07754
Md Nuruddin <nuruddinalways99@gmail.com>
2023-06-15 23:08:35 +0600

    updatd

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 /** Middlewares */
 app.use(cors());
@@ -31,115 +31,97 @@ app.use(express.json());
 //   });
 // };
 
-/** Mongodb client and database connection */
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@todos.ukwfq5e.mongodb.net/?retryWrites=true&w=majority`;
+// Replace the connection URL with your MongoDB Atlas URL
+const atlasUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@todos.ukwfq5e.mongodb.net/Edoofy?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+// Establish connection to MongoDB Atlas
+mongoose.connect(atlasUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-async function run() {
-  try {
-    /** collection from database */
-    const usersCollection = client.db("Edoofy").collection("users");
-    const classesCollection = client.db("Edoofy").collection("classes");
+// Data Modeling
 
-    /** api communication with client side */
-    app.get("/users", async (req, res) => {
-      const decoded = req.decoded;
-      if (decoded?.email !== req.query.email) {
-        return res.send({ error: 1, message: "Forbidden access" });
-      }
-      const result = await usersCollection.find().toArray();
-      res.send(result);
-    });
+const users = mongoose.model("users", new mongoose.Schema({}));
+const classes = mongoose.model("classes", new mongoose.Schema({}));
 
-    app.get("/classes", async (req, res) => {
-      const decoded = req.decoded;
-      if (decoded?.email !== req.query.email) {
-        return res.send({ error: 1, message: "Forbidden access" });
-      }
-      const result = await classesCollection.find().toArray();
-      res.send(result);
-    });
+/** api communication with client side */
+app.get("/users", async (req, res) => {
+  const result = await users.find();
+  res.send(result);
+});
 
-    /** JWT Oparetion */
-    // app.post("/jwt", (req, res) => {
-    //   const user = req.body;
-    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    //     expiresIn: "1h",
-    //   });
-    //   res.send({ token });
-    // });
-
-    app.post("/users", async (req, res) => {
-      const user = req.body;
-      const result = await usersCollection.insertOne(user);
-      res.send(result);
-    });
-
-    app.post("/classes", async (req, res) => {
-      const classInfo = req.body;
-      const result = await classesCollection.insertOne(classInfo);
-      res.send(result);
-    });
-
-    app.patch("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const updatedRole = req.body;
-      const query = { _id: new ObjectId(id) };
-      const updateInfo = {
-        $set: {
-          role: updatedRole.role,
-        },
-      };
-      const result = await usersCollection.updateOne(query, updateInfo);
-      res.send(result);
-    });
-
-    app.patch("/classes/:id", async (req, res) => {
-      if (req.body.feedback) {
-        // const id = req.params.id;
-        // console.log(id);
-        // const query = { _id: new ObjectId(id) };
-        // const updatedFeedback = req.body;
-        // const updateInfo = {
-        //   $set: {
-        //     feedback: updatedFeedback.feedback,
-        //   },
-        // };
-        // const result = await classesCollection.updateOne(query, updateInfo);
-        // res.send(result);
-      } else if (req.body.status) {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const updatedStatus = req.body;
-        const updateInfo = {
-          $set: {
-            status: updatedStatus.status,
-          },
-        };
-        const result = await classesCollection.updateOne(query, updateInfo);
-        res.send(result);
-      } else {
-        ("");
-      }
-    });
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
+app.get("/classes", async (req, res) => {
+  const decoded = req.decoded;
+  if (decoded?.email !== req.query.email) {
+    return res.send({ error: 1, message: "Forbidden access" });
   }
-}
-run().catch(console.dir);
+  const result = await classes.find();
+  res.send(result);
+});
+
+/** JWT Oparetion */
+// app.post("/jwt", (req, res) => {
+//   const user = req.body;
+//   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+//     expiresIn: "1h",
+//   });
+//   res.send({ token });
+// });
+
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
+
+app.post("/classes", async (req, res) => {
+  const classInfo = req.body;
+  const result = await classesCollection.insertOne(classInfo);
+  res.send(result);
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const updatedRole = req.body;
+  const query = { _id: new ObjectId(id) };
+  const updateInfo = {
+    $set: {
+      role: updatedRole.role,
+    },
+  };
+  const result = await usersCollection.updateOne(query, updateInfo);
+  res.send(result);
+});
+
+app.patch("/classes/:id", async (req, res) => {
+  if (req.body.feedback) {
+    // const id = req.params.id;
+    // console.log(id);
+    // const query = { _id: new ObjectId(id) };
+    // const updatedFeedback = req.body;
+    // const updateInfo = {
+    //   $set: {
+    //     feedback: updatedFeedback.feedback,
+    //   },
+    // };
+    // const result = await classesCollection.updateOne(query, updateInfo);
+    // res.send(result);
+  } else if (req.body.status) {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const updatedStatus = req.body;
+    const updateInfo = {
+      $set: {
+        status: updatedStatus.status,
+      },
+    };
+    const result = await classesCollection.updateOne(query, updateInfo);
+    res.send(result);
+  } else {
+    ("");
+  }
+});
 
 app.get("/", (req, res) => res.send("Edoofy Server is running"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
